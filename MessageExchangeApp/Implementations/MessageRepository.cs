537a7760d4626eef77021843b5bde3902d60b72b
Davron Yusupov <davrony@synel.co.il>
2024-08-28 19:05:33 +0500

    feat: removed orm Dapper from the project

--- MessageExchangeApp/Implementations/MessageRepository.cs
+++ MessageExchangeApp/Implementations/MessageRepository.cs
@@ -1,5 +1,4 @@
-﻿using Dapper;
-using MessageExchangeApp.Interfaces;
+﻿using MessageExchangeApp.Interfaces;
 using MessageExchangeApp.Models;
 using System.Data;
 
@@ -19,30 +18,95 @@ namespace MessageExchangeApp.Implementations
         public async Task<Message> AddMessageAndReturnAsync(string content, int sequentialNumber)
         {
             var sql = @"
-            INSERT INTO Messages (Text, SequenceNumber, Timestamp)
-            VALUES (@Text, @SequentialNumber, @Timestamp)
-            RETURNING Id, Text, SequenceNumber, Timestamp;";
+                INSERT INTO Messages (Text, SequenceNumber, Timestamp)
+                VALUES (@Text, @SequentialNumber, @Timestamp)
+                RETURNING Id, Text, SequenceNumber, Timestamp;";
 
-            var parameters = new
+            using (var command = _dbConnection.CreateCommand())
             {
-                Text = content,
-                SequentialNumber = sequentialNumber,
-                Timestamp = DateTime.UtcNow
-            };
+                command.CommandText = sql;
+                command.CommandType = CommandType.Text;
 
-            var result = await _dbConnection.QuerySingleAsync<Message>(sql, parameters);
-            _logger.LogInformation($"Added message into databse with text: {result.Text}, sequence number: {result.SequenceNumber} at {result.Timestamp}");
+                var textParam = command.CreateParameter();
+                textParam.ParameterName = "@Text";
+                textParam.Value = content;
+                command.Parameters.Add(textParam);
 
-            return result;
+                var seqNumParam = command.CreateParameter();
+                seqNumParam.ParameterName = "@SequentialNumber";
+                seqNumParam.Value = sequentialNumber;
+                command.Parameters.Add(seqNumParam);
+
+                var timestampParam = command.CreateParameter();
+                timestampParam.ParameterName = "@Timestamp";
+                timestampParam.Value = DateTime.UtcNow;
+                command.Parameters.Add(timestampParam);
+
+                _dbConnection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        var message = new Message
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Text = reader.GetString(reader.GetOrdinal("Text")),
+                            SequenceNumber = reader.GetInt32(reader.GetOrdinal("SequenceNumber")),
+                            Timestamp = reader.GetDateTime(reader.GetOrdinal("Timestamp"))
+                        };
+
+                        _logger.LogInformation($"Added message into database with text: {message.Text}, sequence number: {message.SequenceNumber} at {message.Timestamp}");
+                        return message;
+                    }
+                }
+                _dbConnection.Close();
+            }
+
+            return null;
         }
+
 
         public async Task<IEnumerable<Message>> GetMessagesByDateRangeAsync(DateTime fromDate, DateTime toDate)
         {
             var sql = "SELECT * FROM Messages WHERE Timestamp >= @FromDate AND Timestamp <= @ToDate ORDER BY Timestamp;";
-            var result = await _dbConnection.QueryAsync<Message>(sql, new { FromDate = fromDate, ToDate = toDate });
+            var messages = new List<Message>();
 
-            _logger.LogInformation($"Requested messages from: {fromDate} to: {toDate}");
-            return result;
+            using (var command = _dbConnection.CreateCommand())
+            {
+                command.CommandText = sql;
+                command.CommandType = CommandType.Text;
+
+                var fromDateParam = command.CreateParameter();
+                fromDateParam.ParameterName = "@FromDate";
+                fromDateParam.Value = fromDate;
+                command.Parameters.Add(fromDateParam);
+
+                var toDateParam = command.CreateParameter();
+                toDateParam.ParameterName = "@ToDate";
+                toDateParam.Value = toDate;
+                command.Parameters.Add(toDateParam);
+
+                _dbConnection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var message = new Message
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Text = reader.GetString(reader.GetOrdinal("Text")),
+                            SequenceNumber = reader.GetInt32(reader.GetOrdinal("SequenceNumber")),
+                            Timestamp = reader.GetDateTime(reader.GetOrdinal("Timestamp"))
+                        };
+
+                        messages.Add(message);
+                    }
+                }
+                _logger.LogInformation($"Requested messages from: {fromDate} to: {toDate}");
+                _dbConnection.Close();
+            }
+
+            return messages;
         }
     }
 }
